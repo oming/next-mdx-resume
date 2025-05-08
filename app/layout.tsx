4204f32c9144284b1619c63670c3bcb114f1aaa6
oming <oming.kr@gmail.com>
2025-05-08 15:59:54 +0900

    GoogleAnalytics 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 
@@ -45,6 +47,10 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      {process.env.NODE_ENV === "production" &&
+        process.env.NEXT_PUBLIC_GA_ID && (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID as string} />
+        )}
     </html>
   );
 }
